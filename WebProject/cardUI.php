968ba3664dadccb2b97b2dbaea0ccedf5e25dd33
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2022-12-05 14:57:39 -0300

    Card UI Responsive
    Aqui eu criei o Card UI que será usado para apresentar os Devs buscados

---
+++ WebProject/cardUI.php
@@ -0,0 +1,24 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/card.css">
+    <title>Card UI</title>
+</head>
+<body>
+    <div class="card">
+        <div class="ImgBox">
+            <img src="img/img1.png" alt="foto do desenvolvedor">
+        </div>
+        <div class="content">
+            <div class="details">
+                <h2>João Pedro<br><span>Front-End Developer</span></h2>
+                <div class="actionBtn">
+                    <button>Perfil</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
